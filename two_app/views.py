e880f4913df273dd115055360871e1f4fb0378cb
chao.guan <guanchao@qssec.com>
2016-01-14 20:08:35 +0800

    [优化]将获得数据，filter函数修改为get_or_create

--- two_app/views.py
+++ two_app/views.py
@@ -5,13 +5,9 @@ from models import *
 # Create your views here.
 def index(request):
     ip = request.environ.get('REMOTE_ADDR', None)
-
-    web_user_list = WebUser.objects.filter(ip=ip)
-    if not web_user_list:
-        WebUser.objects.create(ip=ip)
-    else:
-        web_user = web_user_list.get()
+    web_user, created = WebUser.objects.get_or_create(ip=ip)
+    if not created:
         web_user.pv += 1
         web_user.save()
 
-    return render(request, '2.html', {'data': web_user_list[0]})
+    return render(request, '2.html', {'data': web_user})
